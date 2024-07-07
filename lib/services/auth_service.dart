7a92c3cfd4d5a4e8c550e9faba934c7427eb8f96
koufide <koufide@gmail.com>
2024-07-07 14:53:38 +0000

    ok connexion

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,7 +5,8 @@ import 'package:http/http.dart' as http;
 //CREER UNE CONSTANTE POUR LA BASE URL
 class AuthService {
   // final String baseUrl = 'http://localhost:3000';
-  final String baseUrl = 'http://0.0.0.0:3000';
+  // final String baseUrl = 'http://192.168.56.1:3000'; // carte ethernet
+  final String baseUrl = 'http://192.168.73.44:3000'; // carte wifi
   // final String baseUrl = 'http://10.225.83.66:3000';
 
   final response = null;
@@ -37,11 +38,15 @@ class AuthService {
       //   print("==> AuthService.signIn =>> $response.statusCode ");
       // }
       return null;
-    } catch (e) {
-      print("==> AuthService.signIn Erreur =>> ");
+    } on http.ClientException catch (e) {
+      print("==> AuthService.signIn ClientException Probleme de reseaux =>> ");
       // print("==> AuthService.signIn Response =>> $response");
       // print(e.toString());
       print(e);
+      return null;
+    } catch (e, s ){
+      print("==> AuthService.signIn Exception details =>> $e");
+      print("==> AuthService.signIn Exception STack trace =>> $s");
       return null;
     }
   } // signIn
