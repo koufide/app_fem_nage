76d9843d4195c9757a9ea8fa13eb3f68a50cb976
koufide <koufide@gmail.com>
2024-07-07 21:40:56 +0000

    counter with bloc okok

--- lib/counter/view/counter_page.dart
+++ lib/counter/view/counter_page.dart
@@ -1,25 +1,31 @@
-
 import 'package:app_fem_nage/counter/counter.dart';
 import 'package:app_fem_nage/counter/view/counter_view.dart';
 // import 'package:app_fem_nage/providers/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({super.key});
 
   @override
-  Widget build(BuildContext  context) {
+  Widget build(BuildContext context) {
     // return BlocProvider(
     //   create: (_) => CounterCubit(),
     //   child: const CounterView(),
     // );
 
-    return MultiBlocProvider(
-        providers: [
-          BlocProvider(create: (_) => CounterCubit() ),
-        ],
-        child: const CounterView(),
+    return ResponsiveSizer(
+      builder: (context, orientation, screenType) {
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (_) => CounterCubit()),
+          ],
+          child: const CounterView(),
+        );
+      },
+      maxMobileWidth: 900, //optional
+      maxTabletWidth: 900, //optional
     );
   } //build
 } //CounterPage
