ee033753041c56c7f75a7b3383e1ccab81f5f447
KOUFIDE FIDELINUX <koufide@gmail.com>
2024-07-23 19:17:27 +0000

    test passed OK group ok

--- test/backend_rest_test.dart
+++ test/backend_rest_test.dart
@@ -3,32 +3,43 @@ import 'package:app_fem_nage/rest/backend_rest_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main(){
+  group("test backend_rest_service",
+      () {
+        test("Test backend rest login", () async {
+          final res = await BackendRestService()
+              .signIn(mobile: "+2250700000003",
+              password: "123456789");
 
-  test("Test backend rest login", () async {
-      final res = await BackendRestService()
-          .signIn(mobile: "+2250700000003",
-          password: "123456789");
+          print("Body Response => $res");
 
-      print("Body Response => $res");
+          expect(res is Session, true);
+          // expect(res.user?.id, "71m4vndAh8usU7ib8yDK");
+          // expect(res.user?.nom, "innocent");
+          // expect(res.user?.email, "innocent@gmail.com");
 
-      expect(res is Session, true);
-      // expect(res.user?.id, "71m4vndAh8usU7ib8yDK");
-      // expect(res.user?.nom, "innocent");
-      // expect(res.user?.email, "innocent@gmail.com");
+        });
 
-  });
+        test("Test backend rest login", () async {
+          final res = await BackendRestService()
+              .signIn(mobile: "+2250700000003AAA",
+              password: "123456789AAA");
 
-  test("Test backend rest login", () async {
-    final res = await BackendRestService()
-        .signIn(mobile: "+2250700000003AAA",
-        password: "123456789AAA");
+          print("Body Response => $res");
 
-    print("Body Response => $res");
+          expect(
+            res,
+            isNull,
+          );
+        });
 
-    expect(
-         res,
-      isNull,
-    );
-  });
+      },
+
+
+  );
+
+
+
+
+
 
 }
