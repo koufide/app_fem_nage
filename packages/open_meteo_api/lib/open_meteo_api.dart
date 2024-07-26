d7b7268554a572f157deb46779b25693bbb99384
koufide <koufide@gmail.com>
2024-07-26 00:41:20 +0000

    test ok a suivre

--- packages/open_meteo_api/lib/open_meteo_api.dart
+++ packages/open_meteo_api/lib/open_meteo_api.dart
@@ -1,8 +1,11 @@
 library open_meteo_api;
 
-export 'src/models/models.dart';
 // /// A Calculator.
 // class Calculator {
 //   /// Returns [value] plus 1.
 //   int addOne(int value) => value + 1;
 // }
+
+
+export 'src/models/models.dart';
+export 'src/open_meteo_api_client.dart';
